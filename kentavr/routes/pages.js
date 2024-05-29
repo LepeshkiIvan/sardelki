6efc18e3dfde9fac16ffbd3b743ec41e3d1a0ee3
NotGonnaLetThatPoopSlide <beganikita@yandex.ru>
2024-05-29 21:31:58 +0300

    fart

--- kentavr/routes/pages.js
+++
@@ -1,9 +0,0 @@
-const { sendIndex, sendDashboard } = require("../controllers/auth");
-const { checkAuth, checkCookiesJWT } = require("../middlewares/auth");
-
-const pagesRouter = require("express").Router();
-
-pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
-pagesRouter.get("/", sendIndex);
-
-module.exports = pagesRouter;
